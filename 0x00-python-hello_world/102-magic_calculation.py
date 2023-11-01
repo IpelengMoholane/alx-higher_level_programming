5af4bd2964164e429953b04bef18e1d88ec5db42
Ipeleng Moholane <ipelengportia06@gmail.com>
2023-11-01 12:10:41 -0700

    Python

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,5 +1,10 @@
+#!/usr/bin/python3
 def magic_calculation(a, b):
-    result = 98 ** a
-    result += b
-    return result
+    result = None
 
+    if a > b:
+        result = a + b
+    else:
+        result = a - b * 2
+
+    return result
