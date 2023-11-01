38ed2b25a688f2cc3920c3a03ce64f2ef7639ec4
Ipeleng Moholane <ipelengportia06@gmail.com>
2023-11-01 13:32:32 -0700

    Python

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python3
-remove_char_at = __import__('101-remove_char_at').remove_char_at
-
-print(remove_char_at("Best School", 3))
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))
+#!/usr/bin/python3
+for i in range(122, 96, -1):
+    if i % 2:
+        i = i - 32
+    print("{:c}".format(i), end="")
